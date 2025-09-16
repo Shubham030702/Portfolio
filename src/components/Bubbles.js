88cac56f3a9873101668bb48372a0a36713b8447
Shubham030702 <dangwalshubham10@gmail.com>
2025-09-16 22:17:43 +0530

    second commit

---
+++ src/components/Bubbles.js
@@ -0,0 +1,21 @@
+import "../styles/Bubbles.css"
+
+export default function Bubbles(){
+    return(
+        <div className="boxcontainer">
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+        </div>
+    )
+}
---
+++ src/components/Intro.js
@@ -0,0 +1,15 @@
+export default function Intro1() {
+  return (
+    <section className="introduction">
+        <img src="/Assets/Shubham Dangwal.png" alt="" />
+        <div className="introcontent">
+        Hi, I’m Shubham Dangwal — a Computer Science graduate specializing in AI & ML.
+        I build scalable web apps and intelligent systems using C/C++, Python, JavaScript, React, Node.js, Express, MongoDB, MySQL, TensorFlow and Keras.
+        Notable work: Online coding platform (MERN + Judge0), social media app with Cloudinary, and real-time facial emotion detection.
+        Highlights: Global Rank 1534 in TCS CodeVita, 500+ coding problems solved across LeetCode, CSES & GFG.
+        </div>
+    </section>
+  )
+}
+
+
